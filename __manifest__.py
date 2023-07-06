47392a659c88ad6dca4f2b648f74c7a8200af245
jeffrepo <jeffer.leo93@gmail.com>
2023-07-06 12:00:29 -0600

    UPDATE

---
+++ __manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Ici Makis Extra",
+    'summary': """
+        Ici Makis Extras""",
+    'description': """
+       Ici Makis Extras
+    """,
+    'author': "ST",
+    'website': "",
+    'category': 'Point of Sale',
+    'version': '0.1',
+    'depends': ['base','point_of_sale'],
+    'data': [
+    ],
+    'demo': [
+    ],
+    'assets': {
+        'point_of_sale.assets': [
+            'icimakis/static/src/xml/**/*',
+        ],
+    },
+    'installable': True,
+    'aplication' : False,
+}
